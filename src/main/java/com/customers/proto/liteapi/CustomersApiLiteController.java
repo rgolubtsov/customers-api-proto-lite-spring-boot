f4c1c15560a66ff6d3a4db17fa47914d02cc505e
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2024-09-15 23:35:00 +0300

    Introduce REST controller of the microservice (as a stub for a while).

---
+++ src/main/java/com/customers/proto/liteapi/CustomersApiLiteController.java
@@ -0,0 +1,28 @@
+/*
+ * src/main/java/com/customers/proto/liteapi/CustomersApiLiteController.java
+ * ============================================================================
+ * Customers API Lite microservice prototype. Version 0.0.9
+ * ============================================================================
+ * A Spring Boot-based application, designed and intended to be run
+ * as a microservice, implementing a special Customers API prototype
+ * with a smart yet simplified data scheme.
+ * ============================================================================
+ * (See the LICENSE file at the top of the source tree.)
+ */
+
+package com.customers.proto.liteapi;
+
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+
+/**
+ * The controller class of the microservice.
+ *
+ * @version 0.0.9
+ * @since   0.0.9
+ */
+@WebEndpoint(id="info")
+public class CustomersApiLiteController {
+    // TODO: Populate controller with REST endpoints.
+}
+
+// vim:set nu et ts=4 sw=4:
