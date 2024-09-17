2d93578a91fdbb2cd3a48e4036f97e41582f6113
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2024-09-17 23:25:10 +0300

    Fix inconsistency between REST URI paths to clearly distinguish them between REST API calls.

--- src/main/java/com/customers/proto/liteapi/CustomersApiLiteController.java
+++ src/main/java/com/customers/proto/liteapi/CustomersApiLiteController.java
@@ -29,24 +29,24 @@ import static com.customers.proto.liteapi.CustomersApiLiteHelper.*;
 @RequestMapping(SLASH + REST_PREFIX) // <method> /customers -------------------
 public class CustomersApiLiteController {
     /**
-     * The &quot;<code>/customers/new</code>&quot; <b>PUT</b> endpoint.
+     * The &quot;<code>/customers</code>&quot; <b>PUT</b> endpoint.
      * <br />
      * <br />Creates a new customer (puts customer's data to the database
-     * for their search and retrieval).
+     * for their further search and retrieval).
      *
      * @return The <code>ResponseEntity</code> object with a specific
      *         HTTP status code provided (and the response body
      *         in JSON representation in case of request payload is not valid).
      */
-    @PutMapping(SLASH + REST_NEW) // PUT /customers/new -----------------------
+    @PutMapping // PUT /customers ---------------------------------------------
     public String add_customer() {
         // TODO: Implement creating a new customer.
 
-        return (SLASH + REST_NEW);
+        return (O_BRACKET + SLASH + C_BRACKET);
     }
 
     /**
-     * The &quot;<code>/customers/new-contact</code>&quot; <b>PUT</b> endpoint.
+     * The &quot;<code>/customers/{customer_id}/contact</code>&quot; <b>PUT</b> endpoint.
      * <br />
      * <br />Creates a new contact for a given customer (puts a contact,
      * regarding a given customer, to the database
@@ -55,12 +55,12 @@ public class CustomersApiLiteController {
      * @return The <code>ResponseEntity</code> object with a specific
      *         HTTP status code provided (and the response body
      *         in JSON representation in case of request payload is not valid).
-     */
-    @PutMapping(SLASH + REST_NEW_CONTACT) // PUT /customers/new-contact -------
+     */ // PUT /customers/{customer_id}/contact -------------------------------
+    @PutMapping(SLASH + REST_CUST_ID + SLASH + REST_CONTACT)
     public String add_contact() {
         // TODO: Implement creating a new contact.
 
-        return (SLASH + REST_NEW_CONTACT);
+        return (SLASH + REST_CUST_ID + SLASH + REST_CONTACT);
     }
 
     /**
@@ -90,11 +90,11 @@ public class CustomersApiLiteController {
      *         for a single customer (in the response body
      *         in JSON representation).
      */
-    @GetMapping(SLASH + REST_GET) // GET /customers/{customer_id} -------------
+    @GetMapping(SLASH + REST_CUST_ID) // GET /customers/{customer_id} ---------
     public String get_customer() {
         // TODO: Implement retrieving profile details for a customer.
 
-        return (SLASH + REST_GET);
+        return (SLASH + REST_CUST_ID);
     }
 
     /**
@@ -109,12 +109,12 @@ public class CustomersApiLiteController {
      *         associated with a given customer (in the response body
      *         in JSON representation).
      */ // GET /customers/{customer_id}/contacts ------------------------------
-    @GetMapping(SLASH + REST_GET + SLASH + REST_CONTACTS)
+    @GetMapping(SLASH + REST_CUST_ID + SLASH + REST_CONTACTS)
     public String list_contacts() {
         // TODO: Implement retrieving and listing all contacts
         //       for a given customer.
 
-        return (SLASH + REST_GET + SLASH + REST_CONTACTS);
+        return (SLASH + REST_CUST_ID + SLASH + REST_CONTACTS);
     }
 }
 
