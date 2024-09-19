4d612f88be1be62906912b254d806a343f349f5a
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2024-09-19 18:20:00 +0300

    Bump version number to 0.1.1.

--- src/main/java/com/customers/proto/liteapi/CustomersApiLiteController.java
+++ src/main/java/com/customers/proto/liteapi/CustomersApiLiteController.java
@@ -1,7 +1,7 @@
 /*
  * src/main/java/com/customers/proto/liteapi/CustomersApiLiteController.java
  * ============================================================================
- * Customers API Lite microservice prototype. Version 0.1.0
+ * Customers API Lite microservice prototype. Version 0.1.1
  * ============================================================================
  * A Spring Boot-based application, designed and intended to be run
  * as a microservice, implementing a special Customers API prototype
@@ -26,7 +26,7 @@ import static com.customers.proto.liteapi.CustomersApiLiteHelper.*;
 /**
  * The controller class of the microservice.
  *
- * @version 0.1.0
+ * @version 0.1.1
  * @since   0.1.0
  */
 @RestController // <method> /customers ----------------------------------------
