f337f060cd72f82fd09ed8873aa06178dac870d3
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2024-10-18 23:45:20 +0300

    Move SQL queries to the model class of the microservice.

--- src/main/java/com/customers/proto/liteapi/CustomersApiLiteHelper.java
+++ src/main/java/com/customers/proto/liteapi/CustomersApiLiteHelper.java
@@ -52,57 +52,6 @@ public class CustomersApiLiteHelper {
     // The name for the data source bean, used to connect to the database.
     public static final String DATA_SOURCE = "dataSource";
 
-    // The SQL query for retrieving all customer profiles.
-    public static final String SQL_GET_ALL_CUSTOMERS
-        = "select id ," // as 'Customer ID',"
-        + "       name" // as 'Customer Name'"
-        + " from"
-        + "       customers"
-        + " order by"
-        + "       id";
-
-    // The SQL query for retrieving profile details for a given customer.
-    public static final String SQL_GET_CUSTOMER_BY_ID
-        = "select id ," // as 'Customer ID',"
-        + "       name" // as 'Customer Name'"
-        + " from"
-        + "       customers"
-        + " where"
-        + "      (id = ?)";
-
-    // The SQL query for retrieving all contacts for a given customer.
-    public static final String SQL_GET_ALL_CONTACTS
-        = "select phones.contact," // as 'Phone(s)',"
-        + "       emails.contact " // as 'Email(s)'"
-        + " from"
-        + "       contact_phones phones,"
-        + "       contact_emails emails,"
-        + "       customers      cust"
-        + " where"
-        + "      (cust.id = phones.customer_id) and"
-        + "      (cust.id = emails.customer_id) and"
-        + "      (cust.id =                  ?)"
-        + " order by"
-        + "       emails.contact";
-
-    // The SQL queries for retrieving all contacts of a given type
-    // for a given customer.
-    public static final String[] SQL_GET_CONTACTS_BY_TYPE
-        ={"select phones.contact" // as 'Phone(s)'"
-        + " from"
-        + "       contact_phones phones,"
-        + "       customers      cust"
-        + " where"
-        + "      (cust.id = phones.customer_id) and"
-        + "      (cust.id =                  ?)",
-          "select emails.contact" // as 'Email(s)'"
-        + " from"
-        + "       contact_emails emails,"
-        + "       customers      cust"
-        + " where"
-        + "      (cust.id = emails.customer_id) and"
-        + "      (cust.id =                  ?)"};
-
     // REST URI path-related constants.
     public static final String REST_PREFIX    =  "customers";
     public static final String REST_CUST_ID   = "{customer_id}";
